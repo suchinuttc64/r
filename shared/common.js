5dbed70fb676e5c65cf1200e5e1f0d9a1587f857
suchinuttc64 <suchinuttc64@uttc.ac.th>
2026-05-29 16:27:26 +0700

    Update common.js

--- shared/common.js
+++ shared/common.js
@@ -2,7 +2,7 @@
    shared/common.js - ระบบบันทึกคะแนนและ AI Mentor (เชื่อมต่อ Gemini API)
    ========================================================================== */
 const STORAGE_KEY = 'coolTechScores';
-const API_KEY ='AQ.Ab8RN6IQ7BrvPl2jGj0JMPJYlDqf4zBJDWpC_FcOnNKPGT7Jtw'; // <--- ใส่ API Key ของอาจารย์ตรงนี้ครับ
+const API_KEY ='AQ.Ab8RN6K43MQ1URIeglKBSYcQ_fddPcBJ6FovC3iUEdBI0GOHzA'; // <--- ใส่ API Key ของอาจารย์ตรงนี้ครับ
 let studyStartTime = Date.now();
 
 // --- 1. ระบบบันทึกคะแนน ---
